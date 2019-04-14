a72060513f563b7a7637551c195a30f6f2a31ba8
Dave Boehmer <daboehmer@gmail.com>
2019-04-14 22:28:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,7 +61,8 @@ function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
-  } else {cart.splice(0, cart.length)
+  } else {
+    cart.splice(0, cart.length);
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     
   }
