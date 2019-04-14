1598d4bba3a5c22da1bc466a9e561def1e07e2f3
Dave Boehmer <daboehmer@gmail.com>
2019-04-14 21:58:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
   // write your code here
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].itemName === `${item}`) {
-      
+      cart.splice
     }
   }
 }
